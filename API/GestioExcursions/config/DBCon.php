8ff901b11fb4e51e14ff9a390c22d69149a54cdb
NSalas7 <74241559+NSalas7@users.noreply.github.com>
2021-01-22 09:49:48 +0100

    Update DBCon.php

--- API/GestioExcursions/config/DBCon.php
+++ API/GestioExcursions/config/DBCon.php
@@ -4,7 +4,7 @@ class DataBaseCon{
     private $connection;
 
     function conn(){
-        $this ->connection = new mysqli("127.0.0.1:3308", "root", "", "GestioExcursions");
+        $this ->connection = new mysqli("52.157.103.150:3306", "admin", "admin7", "GestioExcursions");
         if($this->connection->connect_error){
             die("Connection failed: " . $this->connection->connect_error);
         }
