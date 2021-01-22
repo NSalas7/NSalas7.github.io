f8fdd9a8bde5c43362183032892723d0d9ee3008
NSalas7 <74241559+NSalas7@users.noreply.github.com>
2021-01-22 10:17:31 +0100

    Update Profes.php

--- API/GestioExcursions/api/users/Profes.php
+++ API/GestioExcursions/api/users/Profes.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
-$servername = "52.157.103.150";
+$servername = "10.0.1.4";
 $username = "admin";
 $password = "admin7";
 $dbname = "GestioExcursions";
