b41c6439b1bf95afba8ae0d8bbce9736b1ab0496
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-01-26 13:27:17 +0100

    Add files via upload

---
+++ Projecte_Gestio/PUBLIC/inserir_excursio.php
@@ -0,0 +1,29 @@
+<?php
+include '../CONFIG/DBcon.php';
+
+$nomI = $_GET["nom"];
+$llocI = $_GET["lloc"];
+$cursI = $_GET["curs"];
+$duracioI = $_GET["duracio"];
+$d_iniciI = $_GET["d_inici"];
+$descripcioI = $_GET["descripcio"];
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Excursions (Nom, Lloc, Cursos, Duracio, Data_Inici, Descripcio) VALUES ('$nomI', '$llocI', '$cursI', '$duracioI', '$d_iniciI', '$descripcioI')" ;
+
+if ($conn->query($sql) === TRUE) {
+    echo "S'ha afegit la excursio correctament. ";
+}
+echo "<p/>";
+echo "<button class='btn btn-dark'>";
+echo "<a href='BDexcursions.php'>Tornar</a>";
+echo "</button>";
+
+mysqli_close($conn);
