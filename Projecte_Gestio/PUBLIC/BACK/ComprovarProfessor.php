143cfe5f138a27573770ccb7b63998daac5dfeb5
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-02-09 13:19:14 +0100

    Update ComprovarProfessor.php

--- Projecte_Gestio/PUBLIC/BACK/ComprovarProfessor.php
+++ Projecte_Gestio/PUBLIC/BACK/ComprovarProfessor.php
@@ -9,7 +9,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$ssql = "select * from pofessor where correu = '" . $_POST["usuari"] . "' and contrasenya='" . $_POST["contra"] . "'";
+$ssql = "select * from professor where correu = '" . $_POST["usuari"] . "' and contrasenya='" . $_POST["contra"] . "'";
 
 echo $ssql;
 
@@ -19,11 +19,11 @@ if (mysqli_num_rows($rs)==1){
    //TODO CORRECTO!! He detectado un usuario
    $usuari_trobat = mysqli_fetch_object($rs);
    echo "Autenticado correctamente";
-   header ("Location: http://localhost/Disseny/Projecte_Gestio/PUBLIC/BACK/AfegirExcursio.html");
-   
+   header ("Location: http://admin.gestioexcursions.me/Pagina_Principal.php");
+
 }else{
    echo "Error de autenticació!";
-   echo "<p><a href='http://localhost/Disseny/Projecte_Gestio/PUBLIC/BACK/AfegirAlumne.php'>Volver</a>";
+   header ("Location: http://admin.gestioexcursions.me");
 }
 
 
