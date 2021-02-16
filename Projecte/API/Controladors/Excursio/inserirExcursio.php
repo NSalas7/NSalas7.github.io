fd87afd44cd2d9a9b7aebc66981ffba21f52ff90
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-02-16 09:53:29 +0100

    Update inserirExcursio.php

--- Projecte/API/Controladors/Excursio/inserirExcursio.php
+++ Projecte/API/Controladors/Excursio/inserirExcursio.php
@@ -30,5 +30,3 @@ $ex->inserir($nomI,$duracioI,$descripcioI,$publica,$bus,$imatge,$cursI,$llocI,$d
 $db_con->close();
 
 header("Location: http://admin.gestioexcursions.me/MostrarExcursio.php"");
-
-//header("Location: http://localhost/Projecte/Back/Vistes/Cards.php");
