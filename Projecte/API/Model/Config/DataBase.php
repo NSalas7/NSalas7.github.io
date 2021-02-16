72c09a6cb8adc271b1e901dcc474c2bfa651e2fd
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-02-16 09:49:06 +0100

    Update DataBase.php

--- Projecte/API/Model/Config/DataBase.php
+++ Projecte/API/Model/Config/DataBase.php
@@ -5,7 +5,7 @@ class DataBase{
     private $connection;
 
     function conn(){
-        $this ->connection = new mysqli("127.0.0.1:3308", "adminXavi", "12345678", "GestioExcursions");
+        $this ->connection = new mysqli("10.0.1.4", "admin", "admin7", "GestioExcursions");
         if($this->connection->connect_error){
             die("Connection failed: " . $this->connection->connect_error);
         }
