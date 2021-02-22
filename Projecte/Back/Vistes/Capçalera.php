3ea3f1cf77fa4bc5031727ec148f713310716a82
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-02-22 12:25:37 +0100

    Add files via upload

---
+++ Projecte/Back/Vistes/Capçalera.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light" id="colornavbar">
+  <a class="navbar-brand" href="mostrarExcursions"><img src="IMG/logo_negre.png" class="navbar-brand d-inline-block align-top" id="capçaleraBackCards"></a>
+
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent"><br>
+
+    <a href="mostrarExcursions" id="linkExcursions"> Excursions <img src="IMG/mochila.png" alt="mochila" id="mochilaBoto"></a>
+
+    <input type="date" id="calendari" name="calendari">
+
+    <!-- Ens du al DataTable de els professors -->
+    <a href="mostrarProfessors" id="mostrarProfessor"> Professors <i class="fa fa-user"></i></a>
+
+    <!-- Ens du al DataTable de els alumnes -->
+    <a href="mostrarAlumnes" id="mostrarAlumnes"> Alumnes <i class="fa fa-graduation-cap"></i></a>
+
+    <!-- Ens du al DataTable de els pagaments efectuats -->
+    <a href="mostrarPagaments" id="mostrarPagos"> Pagaments <i class="fa fa-money"></i></a>
+
+    <!-- Ens du a la pàgina de Login -->
+    <a href="/" id="tancarSessio"> Tancar Sessió <i class="fa fa-sign-out"></i></a>
+
+  </div>
+</nav>
---
+++ Projecte/Back/Vistes/FormulariLogin.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Gestio Excursions</title>
+
+    <!-- CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="Estils/fullestil1.css">
+
+    <!-- JS -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+
+    <script src="../Public/Js/login.js"></script>
+
+    <!-- Logo del nom de la pagina -->
+    
+    <link rel="icon" type="image/png" href="IMG/logo1_negre.png">
+
+</head>
+
+<!-- Imatge de fons. -->
+<body background="IMG/forestbridge.jpg">
+
+    <?php include_once 'CapçaleraLogin.html'; ?>
+
+    <div class="container" id="contenidorIniciSessio">
+        <h1 id="titol">Inici Sessió</h1>
+
+    <!-- Formulari per donar d'alta un Alumne. -->
+    <form action="" method="post" id="formul">
+        <div class="form-row">
+            <div class="form-group mx-auto">
+                <div id="us"><h5>Usuari:</h5>
+                    <input type="text" class="form-control falu" id="user"  name="usuari" placeholder="@ e-mail" required>
+                </div>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group mx-auto">
+                <div id="co"><h5>Contrasenya:</h5>
+                    <input type="password" class="form-control falu" id="contrasenya" name="contra" required>
+                </div>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group mx-auto">
+                <input type="button" onclick="logIn()" class="btn btn-dark" id="binici" value="Iniciar Sessio">
+            </div>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
