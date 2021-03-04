09d999b51dbca6b14db372b116a1f3cb08ceff1e
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-03-04 09:36:47 +0100

    Add files via upload

---
+++ Projecte/API/Controladors/Alumne/veureAlumne.php
@@ -0,0 +1,15 @@
+<?php
+
+include "../../Model/Config/DataBase.php";
+include "../../Model/Alumne.php";
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: *');
+
+$db = new DataBase();
+$db_con = $db->conn();
+$alumne = new Alumne($db_con);
+
+echo $alumne->readAll();
