5e8b8638d128e536f99a373c2436574796be9ed0
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-03-12 05:52:53 +0100

    Add files via upload

---
+++ Projecte/Front/Public/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+$router->define([
+    "" => "../Vistes/index.php",
+    "afegirAlumne" => "../Vistes/AfegirAlumne.php",
+    "mostrarExcursions" => "../Vistes/MostrarExcursio.php",
+    "veureExcursio" => "../Vistes/VeureExcursio.php",
+    "pagarExcursio" => "../Vistes/RealitzarPagament.php"
+]);
