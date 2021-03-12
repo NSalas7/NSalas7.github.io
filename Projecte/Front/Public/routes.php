859b000a9b336b15147f123a5adf5bb4a1fcb9f4
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2021-03-12 05:46:25 +0100

    Delete Projecte/Front directory

--- Projecte/Front/Public/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$router->define([
-    "" => "../Vistes/index.php",
-    "afegirAlumne" => "../Vistes/AfegirAlumne.php",
-    "mostrarExcursions" => "../Vistes/MostrarExcursio.php",
-    "veureExcursio" => "../Vistes/VeureExcursio.php",
-    "pagarExcursio" => "../Vistes/RealitzarPagament.php"
-]);
